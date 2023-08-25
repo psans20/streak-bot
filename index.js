2d9100beadab4216bbf6c456e24438c8e88df810
TarpCityBitch <travistarpley0@gmail.com>
2023-08-25 16:20:47 -0700

    convert to db

--- index.js
+++ index.js
@@ -30,43 +30,31 @@ client.on('messageCreate', async message => {
     const userQuery = await User.findOne({ where: { id: userId } })
     // TODO
     // CHANGE to userQuery
-    if (!userStreaks[userId]) {
+    if (userQuery) {
       console.log(`USER DOESN'T EXIST`)
       const user = {
         id: userId,
         lastRelapseTimestamp: Date.now().toString()
       }
       await User.create({ ...user })
-
-      userStreaks[userId] = {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        lastRelapse: Date.now() // Set lastRelapse to 1 second ago
-      }
     } else {
       await User.update(
         { lastRelapseTimestamp: Date.now().toString() },
         { where: { id: userId } }
-      )
-      userStreaks[userId].days = 0
-      userStreaks[userId].hours = 0
-      userStreaks[userId].minutes = 0
-      userStreaks[userId].lastRelapse = Date.now() // Set lastRelapse to 1 second ago
-      await User.update
+      ) // Set lastRelapse to 1 second ago
     }
 
     message.author.send('Your streak has been reset.')
   } else if (command === 'update') {
     const userId = message.author.id
     const userQuery = await User.findOne({ where: { id: userId } })
-    if (userStreaks[userId] && userStreaks[userId].lastRelapse) {
+    if (userQuery) {
       const currentTime = Date.now()
       const userData = userQuery.dataValues
       const lastRelapse = userData.lastRelapseTimestamp
       // TODO
       // CHANGE TO LAST RELAPSE
-      const timeDifference = currentTime - userStreaks[userId].lastRelapse
+      const timeDifference = currentTime - lastRelapse
       const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
       const hours = Math.floor(
         (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -83,12 +71,7 @@ client.on('messageCreate', async message => {
 
       durationString += `${minutes} minute(s)`
 
-      console.log(durationString, 'DURATION STRING')
-      if (durationString.trim() === '') {
-        message.reply('You do not have a streak yet.')
-      } else {
-        message.reply(`Your streak is ${durationString} since last relapse.`)
-      }
+      message.reply(`Your streak is ${durationString} since last relapse.`)
     } else {
       message.reply('You do not have a streak yet.')
     }
@@ -97,11 +80,6 @@ client.on('messageCreate', async message => {
     const daysToSet = parseInt(args[0])
     const hoursToSet = parseInt(args[1])
     let setTimestamp = Date.now()
-    if (!userStreaks[userId]) {
-      userStreaks[userId] = {
-        lastRelapse: Date.now()
-      }
-    }
 
     if (!isNaN(daysToSet)) {
       userStreaks[userId].lastRelapse -= daysToSet * 1000 * 60 * 60 * 24
@@ -127,7 +105,8 @@ client.on('messageCreate', async message => {
   }
 })
 async function start () {
+  console.log('START')
   await sqlClient.sync({})
   client.login(process.env.TOKEN ?? '')
 }
-start(); 
+start()
