ed4286b9c7bfea9a38d2bc16484f6ca20d8ca171
bluezoopla <59399623+Undraoid@users.noreply.github.com>
2023-08-25 17:15:44 +0100

    !set not working

--- index.js
+++ index.js
@@ -18,83 +18,102 @@ client.once('ready', () => {
 
 
 
+// ...
+
 client.on('messageCreate', async message => {
-    if (message.author.bot) return;
-  
-    const prefix = '!'; // Change this to your desired command prefix
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-  
-    if (command === 'relapse') {
+  if (message.author.bot) return;
+
+  const prefix = '!'; // Change this to your desired command prefix
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'relapse') {
+    const userId = message.author.id;
+
+    if (!userStreaks[userId]) {
+      userStreaks[userId] = {
+        days: 0,
+        hours: 0,
+        lastRelapse: Date.now(), // Store the timestamp of the last relapse command
+      };
+    } else {
+      userStreaks[userId].days = 0;
+      userStreaks[userId].hours = 0;
+      userStreaks[userId].lastRelapse = Date.now(); // Update the lastRelapse time when resetting the streak
+    }
+
+    message.author.send('Your streak has been reset.');
+
+  } else if (command === 'update') {
       const userId = message.author.id;
-  
+      
+      if (userStreaks[userId]) {
+        const currentTime = Date.now();
+        const timeDifference = currentTime - userStreaks[userId].lastRelapse;
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        
+        let durationString = '';
+        
+        if (days > 0) {
+          durationString += `${days} day(s) `;
+        }
+        
+        if (hours > 0) {
+          durationString += `${hours} hour(s) `;
+        }
+        
+        if (minutes > 0 && days === 0) {
+          durationString += `${minutes} minute(s) `;
+        }
+        
+        if (seconds > 0 && hours === 0 && days === 0) {
+          durationString += `${seconds} second(s)`;
+        }
+
+        if (durationString.trim() === '') {
+          message.reply('Your streak is 0 seconds since last relapse.');
+        } else {
+          message.reply(`Your streak is ${durationString}since last relapse.`);
+        }
+      } else {
+        message.reply('You do not have a streak yet.');
+      }
+  } else if (command === 'set') {
+      const userId = message.author.id;
+      const daysToSet = parseInt(args[0]);
+      const hoursToSet = parseInt(args[1]);
+      console.log(`days: ${daysToSet}, hours: ${hoursToSet}`);
+    
       if (!userStreaks[userId]) {
         userStreaks[userId] = {
           days: 0,
           hours: 0,
-          minutes: 0,
-          lastRelapse: Date.now(), // Store the timestamp of the last relapse command
+          lastRelapse: null
         };
-      } else {
-        userStreaks[userId].days = 0;
-        userStreaks[userId].hours = 0;
-        userStreaks[userId].minutes = 0;
-        userStreaks[userId].lastRelapse = Date.now(); // Update the lastRelapse time when resetting the streak
       }
-  
-      message.author.send('Your streak has been reset.');
-  
-    } else if (command === 'update') {
-        const userId = message.author.id;
-        
-        if (userStreaks[userId]) {
-          const days = userStreaks[userId].days;
-          const hours = userStreaks[userId].hours;
-          const minutes = userStreaks[userId].minutes;
-          
-          let durationString = '';
-          if (days > 0) {
-            durationString += `${days} day(s) `;
-          }
-          if (hours > 0) {
-            durationString += `${hours} hour(s) `;
-          }
-          if (minutes > 0) {
-            durationString += `${minutes} minute(s)`;
-          }
+    
+      if (!isNaN(daysToSet)) {
+        userStreaks[userId].days = daysToSet;
+      }
+    
+      if (!isNaN(hoursToSet)) {
+        userStreaks[userId].hours = hoursToSet;
+      }
       
-         
-          
-            message.reply(`Your streak is ${durationString} long.`);
-        } else {
-          message.reply('You do not have a streak yet.');
-        }
-      } else if (command === 'set') {
-        const userId = message.author.id;
-        const daysToSet = parseInt(args[0]);
-        const hoursToSet = parseInt(args[1]);
-      
-        if (!userStreaks[userId]) {
-          userStreaks[userId] = {
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            lastRelapse: null,
-          };
-        }
-      
-        if (!isNaN(daysToSet)) {
-          userStreaks[userId].days = daysToSet;
-        }
-      
-        if (!isNaN(hoursToSet)) {
-          userStreaks[userId].hours = hoursToSet;
-        }
-      
-        message.reply('Streak set successfully.');
-    }
-  });
+      if (userStreaks[userId].days > 0 || userStreaks[userId].hours > 0) {
+        userStreaks[userId].lastRelapse = Date.now();
+      }
+
+      message.reply('Streak set successfully since last relapse.');
+  }
+});
+
+// ...
+
   
   // Replace 'YOUR_BOT_TOKEN' with your actual bot token
-  client.login('MTE0NDQwNDkwODM2MDA3NzM3Mg.GCAQJe.EtmD2dVqQ91VoWVspfVj5of1WGxMelR3wnXhm4');
+  client.login('MTE0NDQwNDkwODM2MDA3NzM3Mg.GqiZs8.G00ozTyxUm9Bz0PZgOTgIhxRZzNalb5ClZmEwM');
   
